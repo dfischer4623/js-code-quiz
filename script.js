1f9f7417279e78a874d3380bde2ce4e41c778a3b
David Fischer <dfischer@fischerltd.com>
2020-09-20 21:21:56 -0400

    commit 007

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ let questionIndex = 0;
 let quizTime;   
 let timer; 
 const timePerQuestion = 15; 
-const outOfTime = 'Quiz over. You ran out of time.';   
+const outOfTime = 'Quiz over. You ran out of time.';
+
+var questions = [
+  
+  {
+    title: "Arrays in JavaScript can store what data type(s) ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title: "Commonly used data types DO NOT include:", 
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+];
 
 // Get High Scores
 let highscores = [];
